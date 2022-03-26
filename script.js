8c6f30edec52f22c423bea7585530b54651f6143
Amelia Deng <aden9801@uni.sydney.edu.au>
2022-03-27 02:39:24 +1100

    added delete button + js

--- script.js
+++ script.js
@@ -14,8 +14,11 @@ const numbers = document.querySelectorAll(".calculator__numbers");
 
 const calcDisplay = document.querySelector("#calculatorDisplay");
 const prevResult = document.querySelector("#previousOperand");
-let currResult = document.querySelector("#currentOperand");
-console.log(currResult);
+console.log(prevResult);
+const currResult = document.querySelector("#currentOperand");
+const limitWarning = document.querySelector(".calculator__limit-warning");
+const deleteNum = document.querySelector("#delete");
+console.log(deleteNum);
 
 // SELECTING NUMBERS
 // const zero = document.querySelector("#num0");
@@ -48,44 +51,96 @@ toggleBtn.addEventListener("click", () => {
     numbers.forEach((btn) =>
         btn.classList.toggle("calculator__numbers--light-mode")
     );
-    currResult.classList.toggle("calculator__previous-result--light-mode");
+    prevResult.classList.toggle("calculator__previous-result--light-mode");
+    currResult.classList.toggle("calculator__current-result--light-mode");
+    deleteNum.classList.toggle("fa-delete-left--light-mode");
 });
 
-// Node.textContent -> Returns / Sets the textual content of an element and all its descendants.
-const calcResultDisplay = (result) =>{
-    calcDisplay.textContent = result;
+// Default current result calcdisplay to 0 -> Fix
+if ((currResult.textContent = "0")) {
+    currResult.textContent = "";
 }
-// alerting the user that they have reached the limit the calcDisplay can display
-const limitCalcDisplay = (numlimit) =>{
-    if (numlimit.length > 10){
-        alert("Calculator display has reached its limits!")
 
+// Node.textContent -> Returns / Sets the textual content of an element and all its descendants.
+const calcResultDisplay = (result) => {
+    calcDisplay.textContent = result;
+};
+
+// alerting the user that they have reached the limit the calcDisplay can display
+const limitCalcDisplay = (numlimit) => {
+    if (numlimit.length > 10) {
+        if (limitWarning.childNodes.length > 0) {
+            limitWarning.removeChild(limitWarning.firstChild);
+        }
+        createElementWithText(
+            "p",
+            `Number limit has been reached`,
+            limitWarning
+        );
     }
-}
+};
+
+// if numbers value is greater than 10, error warning should appear else numbers concatenate
+const currentResult = (num) => {
+    let currentResult = currResult.textContent;
+    if (currentResult.length > 10) {
+        limitCalcDisplay(currentResult);
+    } else {
+        currResult.textContent += num;
+    }
+};
+
+// previousResult-fix
+const previousResult = (num) => {
+    let prevValue = prevResult.textContent;
+    prevValue += currentResult(num);
+};
 
 // getting single values to display on currentResult
 numbers.forEach((btn) =>
-    btn.addEventListener("click", () => (currResult.innerHTML = btn.value))
+    btn.addEventListener("click", () => currentResult(btn.value))
 );
 
 operators.forEach((btn) =>
-    btn.addEventListener("click", () => (currResult.innerHTML = btn.value))
+    btn.addEventListener("click", () => currentResult(btn.value))
 );
 
-// WHEN TOTAL BUTTON IS CLICKED, CORRECT RESULT APPEARS 
-// **NEEDS FIXING => ???????
-total.addEventListener("click", () => {
-    const totalArr = [];
+// needs fixing
+switch (operators) {
+    case "multiply":
+        createElementWithText(
+            "p",
+            `${multiplication(btn.value, btn.value)}`,
+            currResult
+        );
+        break;
+    case "divide":
+        createElementWithText(
+            "p",
+            `${division(btn.value, btn.value)}`,
+            currResult
+        );
+        break;
+    case "subtract":
+        createElementWithText(
+            "p",
+            `${subtraction(btn.value, btn.value)}`,
+            currResult
+        );
+        break;
+    case "addition":
+        createElementWithText(
+            "p",
+            `${addition(btn.value, btn.value)}`,
+            currResult
+        );
+    default:
+        "Invalid Input";
+}
 
-    for (let i = 0; i < totalArr.length; i++) {
-        totalArr[i].addEventListener("click", () => {
-            if (calcResultDisplay(currResult) === "0") {
-                currResult.innerHTML = "";
-            } else {
-                totalArr.push(currResult.innerHTML) = currResult.innerHTML + numbers[i].value;
-            }
-        });
-    }
+// WHEN TOTAL BUTTON IS CLICKED, CORRECT RESULT APPEARS
+// **NEEDS FIXING => ???????
+total.addEventListener("submit", () => {
     // removes values if exceeds one. Only displays one line on currentResult display
     if (currResult.childNodes.length > 0) {
         currResult.removeChild(currResult.firstChild);
